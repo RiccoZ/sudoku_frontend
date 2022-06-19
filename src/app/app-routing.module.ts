b4711671093b6f5ef1d894685ffa830ddf263cd9
Ricco <Ricco.Ziegler@web.de>
2022-06-19 10:20:11 +0200

    initialize

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { StartpageComponent } from "./startpage/startpage.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', redirectTo: '/startpage', pathMatch: 'full'},
+  {path: 'startpage', component: StartpageComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,25 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { StartpageComponent } from './startpage/startpage.component';
+import { HeaderComponent } from './header/header.component';
+import {HttpClientModule} from "@angular/common/http";
+import { SudokutableComponent } from './sudokutable/sudokutable.component';
+import { HttpService } from './services/http.service';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    StartpageComponent,
+    HeaderComponent,
+    SudokutableComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
   ],
-  providers: [],
+  providers: [HttpService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
